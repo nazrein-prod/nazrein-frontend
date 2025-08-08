b2ae28434ddb63a427cab3f12c0857e4a930f94d
grvbrk <gouravbarikofficial@gmail.com>
2025-08-08 18:24:48 +0530

    added logout dropdown

--- apps/user/src/components/UserAuth.tsx
+++ apps/user/src/components/UserAuth.tsx
@@ -2,14 +2,26 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { LogIn, User } from "lucide-react";
-import { useAuth } from "@/providers/auth-provider";
-import Image from "next/image";
+import { LogIn, LogOut, User } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getClientSideSession } from "@/lib/user-client";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function UserAuth() {
-  const { user, isPending } = useAuth();
+  const { data: session, isLoading } = useQuery({
+    queryKey: ["session"],
+    queryFn: getClientSideSession,
+  });
 
-  if (isPending) {
+  if (isLoading) {
     return (
       <div className="flex items-center space-x-2">
         <div className="w-8 h-8 bg-gray-200 rounded-full animate-pulse" />
@@ -18,13 +30,16 @@ export default function UserAuth() {
     );
   }
 
-  if (!user) {
+  if (!session) {
     return (
       <Link
         href="http://localhost:8080/auth/google/login"
         className="flex items-center space-x-2"
       >
-        <Button size="sm" className="cursor-pointer">
+        <Button
+          size="sm"
+          className="cursor-pointer bg-white text-black hover:bg-white hover:text-black"
+        >
           <LogIn size="16" />
           <p>Sign In</p>
         </Button>
@@ -34,60 +49,50 @@ export default function UserAuth() {
 
   return (
     <div className="flex items-center space-x-3">
-      <div className="flex items-center space-x-3">
-        {user.user_image ? (
-          <Image
-            src={user.user_image}
-            alt={user.user_name}
-            width={40}
-            height={40}
-            className="w-8 h-8 rounded-full border border-gray-300"
-          />
-        ) : (
-          <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center">
-            <User className="w-4 h-4 text-gray-600" />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center md:space-x-3 cursor-pointer text-secondary">
+            {session.user_image ? (
+              <Avatar>
+                <AvatarImage src={session.user_image} alt={session.user_name} />
+                <AvatarFallback>NA</AvatarFallback>
+              </Avatar>
+            ) : (
+              <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center">
+                <User className="w-4 h-4 text-muted-foreground" />
+              </div>
+            )}
           </div>
-        )}
-        <div className="hidden md:flex flex-col">
-          <span className="text-sm font-medium text-black">
-            {user.user_name || "User"}
-          </span>
-          <span className="text-xs text-gray-600">{user.user_email}</span>
-        </div>
-      </div>
-
-      {/* <div className="flex items-center space-x-1">
-        <Button
-          variant="ghost"
-          size="sm"
-          className="text-black hover:bg-gray-100"
-          asChild
-        >
-          <Link href="/" title="New Timeline">
-            <Plus size="16" />
-            <span className="hidden md:inline ml-1">New</span>
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="sm"
-          className="text-gray-600 hover:text-black hover:bg-gray-100"
-          asChild
-        >
-          <Link href="/dashboard" title="Dashboard">
-            <Settings size="16" />
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="sm"
-          className="text-gray-600 hover:text-black hover:bg-gray-100"
-          title="Sign Out"
-        >
-          <LogOut size="16" />
-          <Link href="http://localhost:8080/auth/google/logout">Logout</Link>
-        </Button>
-      </div> */}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent sideOffset={10} className="mr-2 md:mr-0">
+          <DropdownMenuLabel className="p-0 font-normal">
+            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={session.user_image} alt={session.user_name} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">
+                  {session.user_name}
+                </span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {session.user_email}
+                </span>
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link
+              href={"http://localhost:8080/auth/google/logout"}
+              className="cursor-pointer"
+            >
+              <LogOut />
+              Log out
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
